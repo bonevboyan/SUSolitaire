ed525ad2e02f3e958e37a56cf84756041bd722de
bonevboyan <boyanbonev43@gmail.com>
2022-06-05 16:57:09 +0300

    Added Sorter

--- src/Model/Common/Sorter.java
+++ src/Model/Common/Sorter.java
@@ -2,8 +2,19 @@ package Model.Common;
 
 import java.util.List;
 
-public class Sorter<E> {
-    public static <E> void sortList(List<E> list) {
+public class Sorter {
+    public static <E extends Comparable<E>> void sortList(List<E> list) {
+        int n = list.size();
+        E temp;
 
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
     }
 }
